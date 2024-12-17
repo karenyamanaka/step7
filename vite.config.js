f2b300bd65d840cbb1cc633a7f2e800e7b643112
Karen <karen@KarennoMacBook-Air.local>
2024-12-17 15:27:44 +0900

    “1217”

--- vite.config.js
+++ vite.config.js
@@ -1,11 +1,23 @@
 import { defineConfig } from 'vite';
 import laravel from 'laravel-vite-plugin';
-
+import vue from '@vitejs/plugin-vue';
 export default defineConfig({
-  server: {
-    hmr: {
-      overlay: false,
+    plugins: [
+        laravel({
+            input: [
+                'resources/js/app.js',
+                'resources/css/app.css',
+            ],
+            refresh: true,
+        }),
+        vue(),
+    ],
+    resolve: {
+        alias: {
+            'vue': 'vue/dist/vue.esm-bundler.js'
+        },
     },
-  },
+    build: {
+        manifest: true,
+    },
 });
-
